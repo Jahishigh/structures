63c3fbdac37f4bdd453168732baea7cb36bca7fb
Jahishigh <maxenceknoderer.mjm@gmail.com>
2017-06-20 17:14:42 +0200

    CSS 3sections + box shadow

--- wp-content/themes/theme_principal/page-templates/front.php
+++ wp-content/themes/theme_principal/page-templates/front.php
@@ -7,8 +7,8 @@ get_header(); ?>
 <div class="section section-header">
 	<div class="row">
 		<h1>Laissez vos <strong>meuble</strong> épouser votre <strong>maison</strong> à la perfection</h1>
-		<p>Ne laissez plus rien <strong>au hasard</strong> en faisant faire vos meubles <strong>sur-mesure</strong> !</p>
-		<a href="#" class="button">Me découvrir</a>
+		<p class="text-header">Ne laissez plus rien <strong>au hasard</strong> en faisant <br/>faire vos meubles <strong>sur-mesure</strong> !</p>
+		<a href="#" class="button bouton-header">Me découvrir</a>
 		<a href="#" class="button">Voir mes créations</a>
 	</div>
 </div>
@@ -17,84 +17,85 @@ get_header(); ?>
 	<div class="row">
 		<h2>Notre savoir faire</h2>
 		<div class="meubles large-4 column">
-			<img src="wp-content/themes/theme_principal/assets/images/meuble-escalier.jpg" alt="" />
+			<img class="img-savoirfaire" src="wp-content/themes/theme_principal/assets/images/meuble-escalier.jpg" alt="" />
 			<h3>Meuble</h3>
-			<p>
+			<p class="text-meuble">
 				Bibliothèques, étagères ou fauteuils, chaque meuble est un projet à part entière ! <strong class="text-purple">L’artisant rencontre toujours le client</strong> afin d’être sur de satisfaire au mieux ses besoins. Tous nos matériaux sont choisies soigneusement pour vous fournir <strong class="text-purple">la meilleure qualité</strong> !
 			</p>
 			<a href="#" class="button">Voir mes créations</a>
 		</div>
 		<div class="petitmobilier large-4 column">
-			<img src="wp-content/themes/theme_principal/assets/images/lampe.png" alt="" />
+			<img class="img-savoirfaire" src="wp-content/themes/theme_principal/assets/images/lampe.png" alt="" />
 			<h3>Petit Mobilier</h3>
-			<p>
+			<p class="text-petitmobilier">
 				Table de chevet, porte revue ou eneintes, nous réalisons aussi de nombreuses <strong class="text-purple">décorations d’intérieure</strong>. Accordez tous vos objets à vos meubles et crée un habitat qui vous <strong class="text-purple">ressemble à la perfection</strong>. Ne laissez plus rien au hasard !</p>
-			<a href="#" class="button">Voir mes créations</a>
+				<a href="#" class="button">Voir mes créations</a>
+			</div>
+			<div class="amenagement large-4 column">
+				<img class="img-savoirfaire" src="wp-content/themes/theme_principal/assets/images/amenagement.jpg" alt="" />
+				<h3>Amenagement</h3>
+				<p>
+					Structures Intérieures <strong class="text-purple">habille votre maison</strong> de la meilleur manière qu’il soit. Qu’il sagisse d’un escalier, d’une armoire ou de grandes tables, nous feront toujours en sortent qu’ils <strong class="text-purple">épousent au mieux les formes de la pièce</strong>. Avec nous, plus aucun espace n’est perdu !</p>
+					<a href="#" class="button">Voir nos projets</a>
+				</div>
+			</div>
 		</div>
-		<div class="amenagement large-4 column">
-			<img src="wp-content/themes/theme_principal/assets/images/amenagement.jpg" alt="" />
-			<h3>Amenagement</h3>
-			<p>
-				Structures Intérieures <strong class="text-purple">habille votre maison</strong> de la meilleur manière qu’il soit. Qu’il sagisse d’un escalier, d’une armoire ou de grandes tables, nous feront toujours en sortent qu’ils <strong class="text-purple">épousent au mieux les formes de la pièce</strong>. Avec nous, plus aucun espace n’est perdu !</p>
-			<a href="#" class="button">Voir nos projets</a>
+		<!-- SECTION SAVOIR FAIRE END -->
+		<div class="section section-création text-center">
+			<div class="row">
+				<h2>Nos creations</h2>
+				<div class="container-creation"></div>
+					<div class="rangement large-3 columns">
+						<img src="wp-content/themes/theme_principal/assets/images/crea-rangement.jpg" alt="" />
+						<h4>Meuble de rangement</h4>
+						<h5 class="top-title">Cuisine</h5>
+					</div>
+					<div class="fauteil large-3 columns">
+						<img src="wp-content/themes/theme_principal/assets/images/crea-fauteil.jpg" alt="" />
+						<h4>Fauteil Rendu Cuir</h4>
+						<h5 class="top-title">Salon-Chambre</h5>
+					</div>
+					<div class="etagere-rouge large-3 columns">
+						<img src="wp-content/themes/theme_principal/assets/images/crea-rouge.jpg" alt="" />
+						<h4>Étagère verre Rouge</h4>
+						<h5 class="top-title">Polyvalent</h5>
+					</div>
+					<div class="escalier large-3 columns">
+						<img src="wp-content/themes/theme_principal/assets/images/crea-escalier.jpg" alt="" />
+						<h4>Escalier Rangement</h4>
+						<h5 class="top-title">Entre étages - Mezzanine</h5>
+					</div>
+					<div class="enceinte large-3 columns">
+						<img src="wp-content/themes/theme_principal/assets/images/crea-enceinte.jpg" alt="" />
+						<h4>Petite Enceinte</h4>
+						<h5>Polyvalent</h5>
+					</div>
+					<div class="dressing large-3 columns">
+						<img src="wp-content/themes/theme_principal/assets/images/crea-dressing.jpg" alt="" />
+						<h4>Dressing</h4>
+						<h5>Chambre</h5>
+					</div>
+				<div class="chevet large-3 columns">
+					<img src="wp-content/themes/theme_principal/assets/images/crea-chevet.jpg" alt="" />
+					<h4>Table de chevet</h4>
+					<h5>Chambre - Salon</h5>
+				</div>
+				<div class="orange large-3 columns">
+					<img src="wp-content/themes/theme_principal/assets/images/crea-orange.jpg" alt="" />
+					<h4>Meuble de rangement</h4>
+					<h5>Cuisine</h5>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-<!-- SECTION SAVOIR FAIRE END -->
-<div class="section section-création text-center">
-	<div class="row">
-		<h2>Nos créations</h2>
-		<div class="rangement large-3 columns">
-			<img src="wp-content/themes/theme_principal/assets/images/crea-rangement.jpg" alt="" />
-			<h4>Meuble de rangement</h4>
-			<h5>Cuisine</h5>
-		</div>
-		<div class="fauteil large-3 columns">
-			<img src="wp-content/themes/theme_principal/assets/images/crea-fauteil.jpg" alt="" />
-			<h4>Fauteil Rendu Cuir</h4>
-			<h5>Salon-Chambre</h5>
-		</div>
-		<div class="etagere-rouge large-3 columns">
-			<img src="wp-content/themes/theme_principal/assets/images/crea-rouge.jpg" alt="" />
-			<h4>Étagère verre Rouge</h4>
-			<h5>Polyvalent</h5>
-		</div>
-		<div class="escalier large-3 columns">
-			<img src="wp-content/themes/theme_principal/assets/images/crea-escalier.jpg" alt="" />
-			<h4>Escalier Rangement</h4>
-			<h5>Entre étages - Mezzanine</h5>
-		</div>
-		<div class="enceinte large-3 columns">
-			<img src="wp-content/themes/theme_principal/assets/images/crea-enceinte.jpg" alt="" />
-			<h4>Petite Enceinte</h4>
-		  <h5>Polyvalent</h5>
-		</div>
-		<div class="dressing large-3 columns">
-			<img src="wp-content/themes/theme_principal/assets/images/crea-dressing.jpg" alt="" />
-			<h4>Dressing</h4>
-			<h5>Chambre</h5>
-		</div>
-		<div class="chevet large-3 columns">
-			<img src="wp-content/themes/theme_principal/assets/images/crea-chevet.jpg" alt="" />
-			<h4>Table de chevet</h4>
-			<h5>Chambre - Salon</h5>
-		</div>
-		<div class="orange large-3 columns">
-			<img src="wp-content/themes/theme_principal/assets/images/crea-orange.jpg" alt="" />
-			<h4>Meuble de rangement</h4>
-			<h5>Cuisine</h5>
-		</div>
-	</div>
-</div>
-<!-- SECTION-CREATION END -->
-<div class="section section-personalisation">
-	<div class="row">
-		<h2>Personalisation</h2>
-					<div class="environnement row">
-						<img src="wp-content/themes/theme_principal/assets/images/environnement.jpg" alt="" class="large-6 column" />
-						<p class="large-6 column">
-							Avec Structures Intérieures, le mot d’ordre est <strong class="text-purple">d’adapter nos meubles à votre environnement</strong>. Pour chaque projet que vous nous apportez, vous rencontrerez nos artisants pour établir votre besoin et les sa tisfaire le mieux possible. Nous nous engagont toujours à <strong class="text-purple">créer le meuble dont vous avez besoin, et que votre maison à besoin</strong>. Vous n’aurez plus besoin de vous adapter au produits du marché, c’est nous qui nous adaptons à <strong class="text-purple">garder l’équilibre de votre maison</strong>.
-						</p>
+		<!-- SECTION-CREATION END -->
+		<div class="section section-personalisation">
+			<div class="row">
+				<h2>Personalisation</h2>
+				<div class="environnement row">
+					<img src="wp-content/themes/theme_principal/assets/images/environnement.jpg" alt="" class="large-6 column" />
+					<p class="large-6 column">
+						Avec Structures Intérieures, le mot d’ordre est <strong class="text-purple">d’adapter nos meubles à votre environnement</strong>. Pour chaque projet que vous nous apportez, vous rencontrerez nos artisants pour établir votre besoin et les sa tisfaire le mieux possible. Nous nous engagont toujours à <strong class="text-purple">créer le meuble dont vous avez besoin, et que votre maison à besoin</strong>. Vous n’aurez plus besoin de vous adapter au produits du marché, c’est nous qui nous adaptons à <strong class="text-purple">garder l’équilibre de votre maison</strong>.
+					</p>
 				</div>
 				<div class="personalisation row">
 					<p class="large-6 column">
@@ -102,86 +103,87 @@ get_header(); ?>
 						Faites toujours confiance à Structures Intérieures, car avant de lancer un projet, <strong class="text-purple">nous apprenons à vous connaitre.</strong>
 					</p>
 					<img src="wp-content/themes/theme_principal/assets/images/personalite.jpg" alt="" class="large-6 column" />
+				</div>
 			</div>
-	</div>
-</div>
-<!-- SECTION PERSONALISATION END -->
-<div class="section section-projets">
-	<div class="row">
-		<h2>Nos projets</h2>
-		<img src="wp-content/themes/theme_principal/assets/images/projet-rouge.jpg" alt="" />
-		<img src="wp-content/themes/theme_principal/assets/images/projet-escalier.jpg" alt="" />
-		<img src="wp-content/themes/theme_principal/assets/images/projet-cuisine.jpg" alt="" />
-		<img src="wp-content/themes/theme_principal/assets/images/projet-rangement.jpg" alt="" />
-		<a href="#" class="button">Nous proposer un projet</a>
-	</div>
-</div>
-<!-- SECTION PROJETS END -->
-<div class="section section-rassure">
-	<div class="row text-center">
-		<h2>Vous êtes le mieux placé <br/> pour en parler</h2>
-		<div class="ras1 large-4 column">
-				<img src="wp-content/themes/theme_principal/assets/images/avatar1.png" alt="" />
-				<p>
-					L’artisant prend réellement son temps pour en apprendre plus sur nous afin de conceptionner le meuble. Un travail impressionnant !
-				</p>
 		</div>
-		<div class="ras2 large-4 column">
-				<img src="wp-content/themes/theme_principal/assets/images/avatar2.png" alt="" />
-				<p>
-					Nous avions besoin de rénover notre salon pour qu’il nous ressemble plus. Et bien grace à Structures Interieures c’est chose faite, bravo !
-				</p>
+		<!-- SECTION PERSONALISATION END -->
+		<div class="section section-projets">
+			<div class="row text-center">
+				<h2>Nos projets</h2>
+				<div class="container-project"></div>
+				<img src="wp-content/themes/theme_principal/assets/images/projet-rouge.jpg" alt="" />
+				<img src="wp-content/themes/theme_principal/assets/images/projet-escalier.jpg" alt="" />
+				<img src="wp-content/themes/theme_principal/assets/images/projet-cuisine.jpg" alt="" />
+				<img src="wp-content/themes/theme_principal/assets/images/projet-rangement.jpg" alt="" />
+				<a href="#" class="button button-project">Nous proposer un projet</a>
+			</div>
 		</div>
-		<div class="ras3 large-4 column">
-				<img src="wp-content/themes/theme_principal/assets/images/avatar3.png" alt="" />
-				<p>
-					Ma femme rêvait d’avoir son dressing personnel. Structures Interieures à réalisé un travail incroyable pour réussir à l’adapter à notre maison. Un grand merci !
-				</p>
+		<!-- SECTION PROJETS END -->
+		<div class="section section-rassure">
+			<div class="row text-center">
+				<h2>Vous etes le mieux place <br/> pour en parler</h2>
+				<div class="ras1 large-4 column">
+					<img src="wp-content/themes/theme_principal/assets/images/avatar1.png" alt="" />
+					<p>
+						L’artisant prend réellement son temps pour en apprendre plus sur nous afin de conceptionner le meuble. Un travail impressionnant !
+					</p>
+				</div>
+				<div class="ras2 large-4 column">
+					<img src="wp-content/themes/theme_principal/assets/images/avatar2.png" alt="" />
+					<p>
+						Nous avions besoin de rénover notre salon pour qu’il nous ressemble plus. Et bien grace à Structures Interieures c’est chose faite, bravo !
+					</p>
+				</div>
+				<div class="ras3 large-4 column">
+					<img src="wp-content/themes/theme_principal/assets/images/avatar3.png" alt="" />
+					<p>
+						Ma femme rêvait d’avoir son dressing personnel. Structures Interieures à réalisé un travail incroyable pour réussir à l’adapter à notre maison. Un grand merci !
+					</p>
+				</div>
+			</div>
 		</div>
-	</div>
-</div>
-<!-- SECTION RASSURE END -->
-<div class="section section-contact">
-	<div class="row">
-		<h2>Nous contacter</h2>
-		<div class="contact-form large-6 column">
-			<div class="champ1" style="width:400px;height:40px;background-color:white;border: 3px solid #25064c;">
-				Votre nom
+		<!-- SECTION RASSURE END -->
+		<div class="section section-contact">
+			<div class="row">
+				<h2>Nous contacter</h2>
+				<div class="contact-form large-6 column">
+					<div class="champ1" style="width:400px;height:40px;background-color:white;border: 3px solid #25064c;">
+						Votre nom
+					</div>
+					<div class="champ2" style="width:400px;height:40px;background-color:white;border: 3px solid #25064c;">
+						Votre e-mail
+					</div>
+					<div class="champ3" style="width:400px;height:40px;background-color:white;border: 3px solid #25064c;">
+						Titre du message
+					</div>
+					<div class="champ4" style="width:400px;height:200px;background-color:white;border: 3px solid #25064c;">
+						Votre message..;
+					</div>
+					<a href="#" class="button">Envoyer</a>
+				</div>
+				<div class="gmap large-6 column" id="map" style="width:630px;height:670px;background-color:red"></div>
 			</div>
-			<div class="champ2" style="width:400px;height:40px;background-color:white;border: 3px solid #25064c;">
-				Votre e-mail
-			</div>
-			<div class="champ3" style="width:400px;height:40px;background-color:white;border: 3px solid #25064c;">
-				Titre du message
-			</div>
-			<div class="champ4" style="width:400px;height:200px;background-color:white;border: 3px solid #25064c;">
-				Votre message..;
-			</div>
-			<a href="#" class="button">Envoyer</a>
 		</div>
-		<div class="gmap large-6 column" id="map" style="width:630px;height:670px;background-color:red"></div>
-	</div>
-</div>
 
-<?php get_footer(); ?>
+		<?php get_footer(); ?>
 
-<script id="__bs_script__">//<![CDATA[
-    document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.12'><\/script>".replace("HOST", location.hostname));
-//]]></script>
+		<script id="__bs_script__">//<![CDATA[
+			document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.12'><\/script>".replace("HOST", location.hostname));
+			//]]></script>
 
-<script>
-	function initMap() {
-		var uluru = {lat: 48.578455, lng: 7.703808};
-		var map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 14,
-			center: uluru
-		});
-		var marker = new google.maps.Marker({
-			position: uluru,
-			map: map
-		});
-	}
-</script>
-<script async defer
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyASTLZyqkStR03yXxdJ3NcuDfwt5zX_cyY&callback=initMap">
-</script>
+			<script>
+			function initMap() {
+				var uluru = {lat: 48.578455, lng: 7.703808};
+				var map = new google.maps.Map(document.getElementById('map'), {
+					zoom: 14,
+					center: uluru
+				});
+				var marker = new google.maps.Marker({
+					position: uluru,
+					map: map
+				});
+			}
+			</script>
+			<script async defer
+			src="https://maps.googleapis.com/maps/api/js?key=AIzaSyASTLZyqkStR03yXxdJ3NcuDfwt5zX_cyY&callback=initMap">
+			</script>
